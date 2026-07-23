119a78b491e1d1218e82daadf174311a1a5b1981
Jake J <36013209+GeoJake@users.noreply.github.com>
2026-07-22 23:12:39 -0400

    Add: final-array-state-after-k-multiplication-operations-i - submission-1

---
+++ Data Structures & Algorithms/final-array-state-after-k-multiplication-operations-i/submission-1.py
@@ -0,0 +1,13 @@
+import heapq
+
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        heap = [(x, i) for i, x in enumerate(nums)]
+        heapq.heapify(heap)
+
+        for _ in range(k):
+            x, i = heapq.heappop(heap)
+            nums[i] *= multiplier
+            heapq.heappush(heap, (nums[i], i))
+        
+        return nums
