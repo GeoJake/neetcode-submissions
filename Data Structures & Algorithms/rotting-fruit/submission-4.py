35503725168845937d0e7737383c8c44bd3a0a31
Jake J <36013209+GeoJake@users.noreply.github.com>
2026-07-31 21:43:25 -0400

    Add: rotting-fruit - submission-4

---
+++ Data Structures & Algorithms/rotting-fruit/submission-4.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        """
+
+        Edge:
+            no rotting fruit
+            rotting fruit cannot reach the fresh fruit
+            no fruit of any kind
+
+        Want to use BFS to track the expansion of rotting at each minute
+
+
+        """
+
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        d = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        q = deque([])
+        total = 0
+        visit = set()
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 2:
+                    q.append((r, c))
+                if grid[r][c] != 0:
+                    total += 1
+        
+        minutes = -1
+
+        while q:
+            print(q)
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in d:
+                    row = r + dr
+                    col = c + dc
+                    if 0 <= row < ROWS and 0 <= col < COLS and not (row, col) in visit and grid[row][col] == 1:
+                        grid[row][col] = 2
+                        visit.add((row, col))
+                        q.append((row, col))
+                visit.add((r, c))
+            minutes += 1 
+        
+        if not total:
+            return 0
+
+        return minutes if total == len(visit) else -1
